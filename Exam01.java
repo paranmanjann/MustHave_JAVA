03fc763c8078da4f2878710010201620b3252df7
paranmanjann <paranmanjann@gmail.com>
2023-06-01 00:50:40 +0000

    변수 사용, 사칙연산

---
+++ Exam01.java
@@ -0,0 +1,15 @@
+class Exam01 {
+  public static void main(String[] args) {
+    // 화면 출력 예제
+
+    // 내용 출력 후 줄 바뀜
+    System.out.println("p");
+
+    // 내용만 출력, 줄 안 바뀜
+    System.out.print("r");
+    System.out.print("m");
+
+    // 내용 없이 줄바꿈
+    System.out.println();
+  }
+}
---
+++ Exam02.java
@@ -0,0 +1,16 @@
+class Exam02 {
+  public static void main(String[] args) {
+    // 계산 결과 출력 예제
+    System.out.println(2 + 3);
+
+    // 변수, 상수, 자료(데이터)형, 대입
+    int num1 = 2;
+    int num2 = 3;
+
+    int result = num1 + num2;
+
+    System.out.println(num1);
+    System.out.println(num2);
+    System.out.println(result);
+  }
+}
