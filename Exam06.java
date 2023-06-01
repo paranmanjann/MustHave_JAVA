b5ddac3ccb3127051f5d98bd8088a2111bc52394
paranmanjann <paranmanjann@gmail.com>
2023-06-01 09:59:13 +0000

    for

---
+++ Exam06.java
@@ -0,0 +1,24 @@
+class Exam06 {
+  public static void main(String[] args) {
+
+    //
+    System.out.print(2 * 1 + " ");
+    System.out.print(2 * 2 + " ");
+    System.out.print(2 * 3 + " ");
+    System.out.print(2 * 4 + " ");
+    System.out.print(2 * 5 + " ");
+    System.out.print(2 * 6 + " ");
+    System.out.print(2 * 7 + " ");
+    System.out.print(2 * 8 + " ");
+    System.out.print(2 * 9 + " ");
+    System.out.println();
+    
+    System.out.println("===============");
+
+    for (int i = 1; i < 10; i = i + 1)
+    {
+      System.out.print(2 * i + " ");
+    }
+    System.out.println();
+  }
+}
