d11d1853111508345f27b5e061e43ef4f5b23758
paranmanjann <paranmanjann@gmail.com>
2023-06-01 09:58:28 +0000

    while

--- Exam05.java
+++ Exam05.java
@@ -1,8 +1,24 @@
 class Exam05 {
   public static void main(String[] args) {
 
+    //
     System.out.println(2 * 1);
-    System.out.println(2 * 1);
+    System.out.println(2 * 2);
+    System.out.println(2 * 3);
+    System.out.println(2 * 4);
+    System.out.println(2 * 5);
+    System.out.println(2 * 6);
+    System.out.println(2 * 7);
+    System.out.println(2 * 8);
+    System.out.println(2 * 9);
+
+    System.out.println("===============");
     
+    int num = 1;
+    while (num < 10)
+    {
+      System.out.println(2 * num);
+      num = num + 1;
+    }
   }
 }
